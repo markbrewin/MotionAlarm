5d65e683c2675b829f0547e8af6b351aa93a8341
Mark <mark.brewin98@gmail.com>
2018-12-18 04:32:04 +0000

    Fetch Users Actual Location
    Fetches the user's current location rather than last known.

--- app/src/main/java/brewin/mark/motionalarm/WakeCheck.java
+++ app/src/main/java/brewin/mark/motionalarm/WakeCheck.java
@@ -4,12 +4,22 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.Date;
 
@@ -22,15 +32,13 @@ public class WakeCheck {
     private FusedLocationProviderClient mFusedLocationClient;
 
     private Date timeOriginal;
-    private int triggerIteration;
+    private int triggerIteration = 0;
 
-    private double lastLat;
-    private double lastLon;
-    private double lastAlt;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
 
-    private double newLat;
-    private double newLon;
-    private double newAlt;
+    private Location oldLocation;
+    private Location newLocation;
 
     public WakeCheck(Context context) {
         Log.d(TAG, "New WakeCheck created");
@@ -47,31 +55,56 @@ public class WakeCheck {
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
 
-        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "Location permission granted.");
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setNumUpdates(1);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-            mFusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                lastAlt = newAlt;
-                                lastLat = newLat;
-                                lastLon = newLon;
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if(locationResult == null) {
+                    Log.d(TAG, "No locationResult retrieved.");
+                } else {
+                    Log.d(TAG, "Location retrieved.");
 
-                                newAlt = location.getAltitude();
-                                newLat = location.getLatitude();
-                                newLon = location.getLongitude();
+                    oldLocation = newLocation;
+                    newLocation = locationResult.getLastLocation();
 
-                                Log.d(TAG, "Phone Location retrieved.");
-                                Log.d(TAG, "Old Location - Alt: " + lastAlt + "\tLat: " + lastLat + "\tLon: " + lastLon);
-                                Log.d(TAG, "New Location - Alt: " + newAlt + "\tLat: " + newLat + "\tLon: " + newLon);
-                            }
-                        }
-                    });
-        } else {
-            Log.d(TAG, "Permissions not granted to access location!");
-        }
+                    //Log.d(TAG, "Old Location - Alt: " + oldLocation.getAltitude() + "\tLat: " +  oldLocation.getLatitude() + "\tLon: " + oldLocation.getLongitude());
+                    Log.d(TAG, "New Location - Alt: " + newLocation.getAltitude() + "\tLat: " +  newLocation.getLatitude() + "\tLon: " + newLocation.getLongitude());
+                }
+            }
+        };
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(context);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                Log.d(TAG, "Requesting location updates.");
+
+                if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "Location permission granted.");
+
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+                } else {
+                    Log.d(TAG, "Permissions not granted to access location!");
+                }
+            }
+        });
+
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "Failed to fetch location.");
+            }
+        });
     }
 }
